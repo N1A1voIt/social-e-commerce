732cdb66e9fbb171a978650b600049fc96859101
RANDRIANARISOA Ny Avohasina Mampandry <120253454+N1A1voIt@users.noreply.github.com>
2025-07-07 12:15:38 +0300

    [feat] - SIgn Up component

--- social-front-end/src/app/main/authentication/signup/signup.component.ts
+++ social-front-end/src/app/main/authentication/signup/signup.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {LoginInputComponent} from "../../../shared/forms/auth/login-input/login-input.component";
+import {PlatformButtonComponent} from "../../../shared/forms/auth/platform-button/platform-button.component";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+    imports: [
+        LoginInputComponent,
+        PlatformButtonComponent
+    ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
--- social-front-end/tailwind.config.js
+++ social-front-end/tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     extend: {
       fontFamily: {
         'custom-font': ['MonaSans', 'sans-serif'], // 'custom-font' is your Tailwind utility, 'MyCustomFont' matches your @font-face name
+      },
+      fontSize: {
+        // base mobile font sizes
+        xl: ['2vh', { lineHeight: '1.75rem' }],    // 20px
+        '2xl': ['3vh', { lineHeight: '2rem' }],     // 24px
+        '3xl': ['4vh', { lineHeight: '2.25rem' }] // 30px
       }
     },
     colors: {
