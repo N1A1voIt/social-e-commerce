1a57bb4dd50555038b820d6ee8f21842ec1f4bd1
RANDRIANARISOA Ny Avohasina Mampandry <120253454+N1A1voIt@users.noreply.github.com>
2025-07-12 16:48:00 +0300

    [feat] - Form container post creation

--- social-front-end/src/app/main/content-management/content-management.component.ts
+++ social-front-end/src/app/main/content-management/content-management.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {SelectResearchComponent} from "./select-research/select-research.component";
 import {PostCardComponent} from "./post-card/post-card.component";
+import {FormContainerComponent} from "../../shared/form-container/form-container.component";
 
 @Component({
   selector: 'app-content-management',
   standalone: true,
   imports: [
     SelectResearchComponent,
-    PostCardComponent
+    PostCardComponent,
+    FormContainerComponent
   ],
   templateUrl: './content-management.component.html',
   styleUrl: './content-management.component.css'
