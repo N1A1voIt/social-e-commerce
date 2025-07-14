c545db34dde0de513dc532cfe44d666450d48e0e
RANDRIANARISOA Ny Avohasina Mampandry <120253454+N1A1voIt@users.noreply.github.com>
2025-07-14 10:58:43 +0300

    Create environment.example.ts

---
+++ social-front-end/src/environments/environment.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    // ...other config
+    firebase: {
+        apiKey: "KI",
+        authDomain: "placeholder.firebaseapp.com",
+        projectId: "project",
+        storageBucket: "placeholder.firebasestorage.app",
+        messagingSenderId: "123456",
+        appId: "appid",
+        measurementId: "faefaez"
+      }
+  };
