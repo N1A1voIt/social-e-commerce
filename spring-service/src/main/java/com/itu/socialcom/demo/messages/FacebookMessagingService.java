a73550a8494391f2b176608af6f2030b0c4eeadd
RANDRIANARISOA Ny Avohasina Mampandry <120253454+N1A1voIt@users.noreply.github.com>
2025-08-05 12:03:29 +0300

    [chore] - Message API fixed for facebook

--- spring-service/src/main/java/com/itu/socialcom/demo/messages/FacebookMessagingService.java
+++ spring-service/src/main/java/com/itu/socialcom/demo/messages/FacebookMessagingService.java
@@ -6,6 +6,8 @@ import com.itu.socialcom.demo.messages.messagechild.MessageChild;
 import com.itu.socialcom.demo.messages.messagechild.MessageChildRepository;
 import com.itu.socialcom.demo.messages.messagemother.MessageMother;
 import com.itu.socialcom.demo.messages.messagemother.MessageMotherRepository;
+import com.itu.socialcom.demo.potentialCustomers.entity.PotentialCustomerV2;
+import com.itu.socialcom.demo.potentialCustomers.repository.PotentialCustomerV2Repository;
 import com.itu.socialcom.demo.socialmedia.entity.ManagedPageCPL;
 import com.itu.socialcom.demo.socialmedia.repository.ManagedPageCPLRepository;
 import org.springframework.stereotype.Service;
@@ -17,11 +19,11 @@ import java.net.URL;
 
 @Service
 public class FacebookMessagingService extends MessageService{
-    public FacebookMessagingService(InboxRepository inboxRepo, MessageMotherRepository motherRepo, MessageChildRepository childRepo, ManagedPageCPLRepository managedPageCPLRepository) {
-        super(inboxRepo, motherRepo, childRepo,managedPageCPLRepository);
+    public FacebookMessagingService(InboxRepository inboxRepo, MessageMotherRepository motherRepo, MessageChildRepository childRepo, ManagedPageCPLRepository managedPageCPLRepository, PotentialCustomerV2Repository potentialCustomerV2Repository) {
+        super(inboxRepo, motherRepo, childRepo,managedPageCPLRepository,potentialCustomerV2Repository);
     }
     @Override
-    public void sendMessage(String recipientId, String messageText, String pageAccessToken) {
+    public void sendMessage(String recipientId, String messageText, String pageAccessToken) throws Exception{
         try {
             String apiUrl = "https://graph.facebook.com/v18.0/me/messages?access_token=" + pageAccessToken;
 
@@ -47,26 +49,30 @@ public class FacebookMessagingService extends MessageService{
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 System.out.println("Message sent successfully.");
             } else {
-                System.out.println("Failed to send message. HTTP error code: " + responseCode);
+                throw new Exception("Failed to send message. HTTP error code: " + responseCode);
             }
 
             conn.disconnect();
         } catch (Exception e) {
             e.printStackTrace();
+            throw e;
         }
     }
 
 
     @Transactional
-    public MessageChild answerMessageChild(MessageBody messageBody) {
+    public MessageChild answerMessageChild(MessageBody messageBody) throws Exception{
         MessageChild messageChild = new MessageChild();
         messageChild.setIdMm( messageBody.getIdMm().intValue());
         messageChild.setFromPlatform(false);
         messageChild.setMessage(messageBody.getMessage());
         super.childRepo.save(messageChild);
         MessageMother messageMother = super.motherRepo.findById(messageBody.getIdMm().intValue()).orElse(null);
-        ManagedPageCPL managedPageCPL = super.managedPageCPLRepository.findByPlatformIdentifierAndPlatform(messageMother.getIdMp().toString(), "facebook");
-        sendMessage(managedPageCPL.getPlatformIdentifier(),messageBody.getMessage(),managedPageCPL.getRefreshToken());
+//        System.out.println("MessageMother: " + messageMother.getIdMp());
+        ManagedPageCPL managedPageCPL = super.managedPageCPLRepository.findById(messageMother.getIdMp().longValue()).orElse(null);
+        System.out.println("ManagedPageCPL: " + managedPageCPL.getPlatformIdentifier() + ", Token: " + managedPageCPL.getRefreshToken());
+        PotentialCustomerV2 potentialCustomerV2 = super.potentialCustomerV2Repository.findById(messageMother.getIdPc()).orElse(null);
+        sendMessage(potentialCustomerV2.getIdentifierOnPlatform(),messageBody.getMessage(),managedPageCPL.getRefreshToken());
         return messageChild;
     }
 }
--- spring-service/src/main/java/com/itu/socialcom/demo/messages/MessageService.java
+++ spring-service/src/main/java/com/itu/socialcom/demo/messages/MessageService.java
@@ -7,6 +7,7 @@ import com.itu.socialcom.demo.messages.messagechild.MessageChild;
 import com.itu.socialcom.demo.messages.messagechild.MessageChildRepository;
 import com.itu.socialcom.demo.messages.messagemother.MessageMother;
 import com.itu.socialcom.demo.messages.messagemother.MessageMotherRepository;
+import com.itu.socialcom.demo.potentialCustomers.repository.PotentialCustomerV2Repository;
 import com.itu.socialcom.demo.socialmedia.repository.ManagedPageCPLRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,17 +22,19 @@ public abstract class MessageService {
     final MessageMotherRepository motherRepo;
     final MessageChildRepository childRepo;
     final ManagedPageCPLRepository managedPageCPLRepository;
-
+    final PotentialCustomerV2Repository potentialCustomerV2Repository;
     public MessageService(
             InboxRepository inboxRepo,
             MessageMotherRepository motherRepo,
             MessageChildRepository childRepo,
-            ManagedPageCPLRepository managedPageCPLRepository
+            ManagedPageCPLRepository managedPageCPLRepository,
+            PotentialCustomerV2Repository potentialCustomerV2Repository
     ) {
         this.inboxRepo = inboxRepo;
         this.motherRepo = motherRepo;
         this.childRepo = childRepo;
         this.managedPageCPLRepository = managedPageCPLRepository;
+        this.potentialCustomerV2Repository = potentialCustomerV2Repository;
     }
 
     public Inbox saveInbox(Inbox inbox) {
@@ -45,8 +48,8 @@ public abstract class MessageService {
     public MessageChild saveChild(MessageChild child) {
         return childRepo.save(child);
     }
-    public abstract void sendMessage(String recipientId, String messageText, String pageAccessToken);
-    public abstract MessageChild answerMessageChild(MessageBody messageBody);
+    public abstract void sendMessage(String recipientId, String messageText, String pageAccessToken) throws Exception;
+    public abstract MessageChild answerMessageChild(MessageBody messageBody) throws Exception;
 
     // Add fetch methods as needed
 }
