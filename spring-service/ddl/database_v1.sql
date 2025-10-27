a12705711174a52e4275a026e6e6f7c716241bf8
RANDRIANARISOA Ny Avohasina Mampandry <120253454+N1A1voIt@users.noreply.github.com>
2025-10-27 22:13:07 +0300

    [fix] - FIx view stock

--- spring-service/ddl/database_v1.sql
+++ spring-service/ddl/database_v1.sql
@@ -735,7 +735,12 @@ CREATE VIEW v_message_box AS
 
 CREATE VIEW v_product_stock_cpl AS
     WITH stock_details AS (
-        SELECT max(action_at),d_product_number,id_product FROM stocks_child GROUP BY id_product, d_product_number
+        SELECT DISTINCT ON (id_product)
+            id_product,
+            d_product_number,
+            action_at
+        FROM stocks_child
+        ORDER BY id_product, created_at DESC
     )
     SELECT
         p.id_product,description,name,price,media,id_seller,c.id_category,
@@ -747,17 +752,23 @@ CREATE VIEW v_product_stock_cpl AS
         LEFT JOIN stock_details s ON p.id_product = s.id_product
         JOIN category c on c.id_category = p.id_category;
 
+-- CREATE VIEW v_variant_cpl AS
+
 CREATE VIEW v_variant_cpl AS
-    WITH stock_details AS (
-        SELECT max(action_at),d_variant_number,id_variant FROM stocks_child GROUP BY id_variant, d_variant_number
-    )
-    SELECT
-        v.id_variant, v.title, v.price, v.created_at, v.updated_at, v.id_product,
-        COALESCE(d_variant_number,0) as variant_number,
-        CASE WHEN COALESCE(d_variant_number,0) = 0 THEN 'Out of Stock'
-                WHEN COALESCE(d_variant_number,0) >= 10 THEN 'In Stock'
-                WHEN COALESCE(d_variant_number,0) > 0 AND COALESCE(d_variant_number,0) < 10 THEN 'Low Stock' END as stock_status
-        FROM variants_v2 v LEFT JOIN stock_details ON v.id_variant = stock_details.id_variant;
+        WITH stock_details AS (
+            SELECT DISTINCT ON (id_variant)
+                id_variant,
+                d_variant_number
+            FROM stocks_child
+            ORDER BY id_variant, created_at DESC
+        )
+        SELECT
+            v.id_variant, v.title, v.price, v.created_at, v.updated_at, v.id_product,
+            COALESCE(d_variant_number,0) as variant_number,
+            CASE WHEN COALESCE(d_variant_number,0) = 0 THEN 'Out of Stock'
+                    WHEN COALESCE(d_variant_number,0) >= 10 THEN 'In Stock'
+                    WHEN COALESCE(d_variant_number,0) > 0 AND COALESCE(d_variant_number,0) < 10 THEN 'Low Stock' END as stock_status
+            FROM variants_v2 v LEFT JOIN stock_details ON v.id_variant = stock_details.id_variant;
 
 
 WITH recent_variants_retriever AS
