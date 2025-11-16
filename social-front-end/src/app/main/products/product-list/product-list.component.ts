bb080251783855fdf3c6c5a7f77bd11cfaf9b6e3
RANDRIANARISOA Ny Avohasina Mampandry <120253454+N1A1voIt@users.noreply.github.com>
2025-11-16 11:27:11 +0300

    refonte produits

--- social-front-end/src/app/main/products/product-list/product-list.component.ts
+++ social-front-end/src/app/main/products/product-list/product-list.component.ts
@@ -1,20 +1,25 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
 import {Product, ProductCpl} from "../products.types";
 import {HttpClient} from "@angular/common/http";
 import {javaHost} from "../../../../environments/environment";
-import {NgForOf} from "@angular/common";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {ProductServiceService} from "../product-service.service";
+import {FrenchNumberPipe} from "../../../shared/french-number.pipe";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgClass,
+    FrenchNumberPipe,
+    NgIf
   ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
+  openMenuProductId: number | null = null;
   @Output() addProductClicked = new EventEmitter<void>();
   @Output() editProductClicked = new EventEmitter<ProductCpl>();
   @Input() productsList: ProductCpl[] = [];
@@ -26,9 +31,30 @@ export class ProductListComponent implements OnInit {
   onAddProduct() {
     this.addProductClicked.emit();
   }
-  
+
   onEditProduct(product: ProductCpl, event: Event) {
     event.stopPropagation();
     this.editProductClicked.emit(product);
+  }
+
+  toggleMenu(productId: number, event: Event) {
+    event.stopPropagation();
+    this.openMenuProductId = this.openMenuProductId === productId ? null : productId;
+  }
+
+  isMenuOpen(productId: number): boolean {
+    return this.openMenuProductId === productId;
+  }
+
+  onDeleteProduct(product: ProductCpl, event: Event) {
+    event.stopPropagation();
+    // Implement delete logic here
+    console.log('Delete product:', product);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    // Close menu when clicking outside
+    this.openMenuProductId = null;
   }
 }
